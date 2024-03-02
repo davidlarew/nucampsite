1fcc545d6cf391d9e930cc6fef3eb59cfa946f71
David Larew <davidlarew@Davids-MacBook-Air.local>
2024-03-02 04:17:51 -0800

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import NucampLogo from './app/assets/img/logo.png';
 //import logo from './logo.svg';
 //import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      Hello Nucamp!
+         <Navbar dark color='primary' sticky='top' expand='md'>
+               <Container>
+                    <NavbarBrand href='/'>
+                        <img src={NucampLogo} alt='nucamp logo' />
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+            I'm ready for workshop!
     </div>
   );
 }
